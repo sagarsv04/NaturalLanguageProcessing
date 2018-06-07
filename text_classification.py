83135cf54ebd8b6c145f1844ca7ef011f8b491fb
sagarsv <sagar.vishwakarma@decimalpointanalytics.com>
2018-06-07 19:55:20 +0530

    added naive bayes classifier

--- text_classification.py
+++ text_classification.py
@@ -8,7 +8,6 @@ documents = [(list(movie_reviews.words(fileid)), category)
 
 random.shuffle(documents)
 
-print(documents[1])
 
 all_words = []
 for w in movie_reviews.words():
@@ -21,12 +20,24 @@ print(all_words["stupid"])
 word_features = list(all_words.keys())[:3000]
 
 def find_features(document):
+	# document = movie_reviews.words('neg/cv000_29416.txt')
 	words = set(document)
 	features = {}
 	for w in word_features:
 		features[w] = (w in words)
-
 	return features
 
 print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+# set that we'll train our classifier with
+training_set = featuresets[:1900]
+
+# set that we'll test against.
+testing_set = featuresets[1900:]
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
+
+classifier.show_most_informative_features(15)
