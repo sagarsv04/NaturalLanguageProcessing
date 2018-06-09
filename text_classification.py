81da899481a63908058db06f35907e7d122cfd48
sagarsv04 <scoutsv.sv@gmail.com>
2018-06-09 22:03:12 +0530

    added Sentiment Analysis method

--- text_classification.py
+++ text_classification.py
@@ -13,6 +13,10 @@ from statistics import mode
 from nltk.tokenize import word_tokenize
 
 
+if not os.path.exists("./pickled/"):
+	os.mkdir("./pickled/")
+
+
 class VoteClassifier(ClassifierI):
 	def __init__(self, *classifiers):
 		self._classifiers = classifiers
@@ -35,42 +39,59 @@ class VoteClassifier(ClassifierI):
 		return conf
 
 
-short_pos = open("./positive.txt","r").read()
-short_neg = open("./negative.txt","r").read()
+if not(os.path.exists("./pickled/documents.pickle") and os.path.exists("./pickled/word_features5k.pickle")):
+	short_pos = open("./positive.txt","r").read()
+	short_neg = open("./negative.txt","r").read()
+	# move this up here
+	all_words = []
+	documents = []
+	# j is adject, r is adverb, and v is verb
+	# allowed_word_types = ["J","R","V"]
+	allowed_word_types = ["J"]
+	for p in short_pos.split('\n'):
+	    documents.append( (p, "pos") )
+	    words = word_tokenize(p)
+	    pos = nltk.pos_tag(words)
+	    for w in pos:
+	        if w[1][0] in allowed_word_types:
+	            all_words.append(w[0].lower())
 
-documents = []
+	for p in short_neg.split('\n'):
+	    documents.append( (p, "neg") )
+	    words = word_tokenize(p)
+	    pos = nltk.pos_tag(words)
+	    for w in pos:
+	        if w[1][0] in allowed_word_types:
+	            all_words.append(w[0].lower())
 
-for r in short_pos.split('\n'):
-    documents.append( (r, "pos") )
+	save_documents = open("./pickled/documents.pickle","wb")
+	pickle.dump(documents, save_documents)
+	save_documents.close()
 
-for r in short_neg.split('\n'):
-    documents.append( (r, "neg") )
+	all_words = nltk.FreqDist(all_words)
 
+	word_features = list(all_words.keys())[:5000]
 
-all_words = []
+	save_word_features = open("./pickled/word_features5k.pickle","wb")
+	pickle.dump(word_features, save_word_features)
+	save_word_features.close()
+else:
+	save_documents_file = open("./pickled/documents.pickle", "rb")
+	documents = pickle.load(save_documents_file)
+	save_documents_file.close()
 
-short_pos_words = word_tokenize(short_pos)
-short_neg_words = word_tokenize(short_neg)
+	word_features_file = open("./pickled/word_features5k.pickle", "rb")
+	word_features = pickle.load(word_features_file)
+	word_features_file.close()
 
-for w in short_pos_words:
-    all_words.append(w.lower())
-
-for w in short_neg_words:
-    all_words.append(w.lower())
-
-all_words = nltk.FreqDist(all_words)
-
-word_features = list(all_words.keys())[:5000]
 
 def find_features(document):
-	# document = movie_reviews.words('neg/cv000_29416.txt')
 	words = word_tokenize(document)
 	features = {}
 	for w in word_features:
 		features[w] = (w in words)
 	return features
 
-# print((find_features(movie_reviews.words('neg/cv000_29416.txt'))))
 
 featuresets = [(find_features(rev), category) for (rev, category) in documents]
 
@@ -82,56 +103,107 @@ training_set = featuresets[:10000]
 # set that we'll test against.
 testing_set = featuresets[10000:]
 
-if not os.path.exists("./naivebayes.pickle"):
+
+if not os.path.exists("./pickled/naivebayes.pickle"):
 	classifier = nltk.NaiveBayesClassifier.train(training_set)
 	print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
-	save_classifier = open("./naivebayes.pickle","wb")
+	save_classifier = open("./pickled/naivebayes.pickle","wb")
 	pickle.dump(classifier, save_classifier)
 	save_classifier.close()
 else:
-	classifier_file = open("./naivebayes.pickle", "rb")
-	classifier = pickle.load(classifier_file)
-	classifier_file.close()
-	print("Classifier accuracy percent:",(nltk.classify.accuracy(classifier, testing_set))*100)
+	open_file = open("./pickled/naivebayes.pickle", "rb")
+	classifier = pickle.load(open_file)
+	open_file.close()
+
 
 classifier.show_most_informative_features(15)
 
-MNB_classifier = SklearnClassifier(MultinomialNB())
-MNB_classifier.train(training_set)
-print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
 
-BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
-BernoulliNB_classifier.train(training_set)
-print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+if not os.path.exists("./pickled/MNB_classifier.pickle"):
+	MNB_classifier = SklearnClassifier(MultinomialNB())
+	MNB_classifier.train(training_set)
+	print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+	save_classifier = open("./pickled/MNB_classifier.pickle","wb")
+	pickle.dump(classifier, save_classifier)
+	save_classifier.close()
+else:
+	open_file = open("./pickled/MNB_classifier.pickle", "rb")
+	MNB_classifier = pickle.load(open_file)
+	open_file.close()
 
-LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
-LogisticRegression_classifier.train(training_set)
-print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
 
-SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
-SGDClassifier_classifier.train(training_set)
-print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+if not os.path.exists("./pickled/BernoulliNB_classifier.pickle"):
+	BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+	BernoulliNB_classifier.train(training_set)
+	print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+	save_classifier = open("./pickled/BernoulliNB_classifier.pickle","wb")
+	pickle.dump(classifier, save_classifier)
+	save_classifier.close()
+else:
+	open_file = open("./pickled/BernoulliNB_classifier.pickle", "rb")
+	BernoulliNB_classifier = pickle.load(open_file)
+	open_file.close()
 
-# SVC_classifier = SklearnClassifier(SVC())
-# SVC_classifier.train(training_set)
-# print("SVC_classifier accuracy percent:", (nltk.classify.accuracy(SVC_classifier, testing_set))*100)
 
-LinearSVC_classifier = SklearnClassifier(LinearSVC())
-LinearSVC_classifier.train(training_set)
-print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+if not os.path.exists("./pickled/LogisticRegression_classifier.pickle"):
+	LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+	LogisticRegression_classifier.train(training_set)
+	print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+	save_classifier = open("./pickled/LogisticRegression_classifier.pickle","wb")
+	pickle.dump(classifier, save_classifier)
+	save_classifier.close()
+else:
+	open_file = open("./pickled/LogisticRegression_classifier.pickle", "rb")
+	LogisticRegression_classifier = pickle.load(open_file)
+	open_file.close()
 
-NuSVC_classifier = SklearnClassifier(NuSVC())
-NuSVC_classifier.train(training_set)
-print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+
+if not os.path.exists("./pickled/SGDClassifier_classifier.pickle"):
+	SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+	SGDClassifier_classifier.train(training_set)
+	print("SGDClassifier_classifier accuracy percent:", (nltk.classify.accuracy(SGDClassifier_classifier, testing_set))*100)
+	save_classifier = open("./pickled/SGDClassifier_classifier.pickle","wb")
+	pickle.dump(classifier, save_classifier)
+	save_classifier.close()
+else:
+	open_file = open("./pickled/SGDClassifier_classifier.pickle", "rb")
+	SGDClassifier_classifier = pickle.load(open_file)
+	open_file.close()
+
+
+if not os.path.exists("./pickled/LinearSVC_classifier.pickle"):
+	LinearSVC_classifier = SklearnClassifier(LinearSVC())
+	LinearSVC_classifier.train(training_set)
+	print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+	save_classifier = open("./pickled/LinearSVC_classifier.pickle","wb")
+	pickle.dump(classifier, save_classifier)
+	save_classifier.close()
+else:
+	open_file = open("./pickled/LinearSVC_classifier.pickle", "rb")
+	LinearSVC_classifier = pickle.load(open_file)
+	open_file.close()
+
+
+if not os.path.exists("./pickled/NuSVC_classifier.pickle"):
+	NuSVC_classifier = SklearnClassifier(NuSVC())
+	NuSVC_classifier.train(training_set)
+	print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+	save_classifier = open("./pickled/NuSVC_classifier.pickle","wb")
+	pickle.dump(classifier, save_classifier)
+	save_classifier.close()
+else:
+	open_file = open("./pickled/NuSVC_classifier.pickle", "rb")
+	NuSVC_classifier = pickle.load(open_file)
+	open_file.close()
 
 
 voted_classifier = VoteClassifier(classifier, NuSVC_classifier, LinearSVC_classifier, SGDClassifier_classifier, MNB_classifier, BernoulliNB_classifier, LogisticRegression_classifier)
 
 
-print("voted_classifier accuracy percent:", (nltk.classify.accuracy(voted_classifier, testing_set))*100)
-print("Classification:", voted_classifier.classify(testing_set[0][0]), "Confidence %:",voted_classifier.confidence(testing_set[0][0])*100)
-print("Classification:", voted_classifier.classify(testing_set[1][0]), "Confidence %:",voted_classifier.confidence(testing_set[1][0])*100)
-print("Classification:", voted_classifier.classify(testing_set[2][0]), "Confidence %:",voted_classifier.confidence(testing_set[2][0])*100)
-print("Classification:", voted_classifier.classify(testing_set[3][0]), "Confidence %:",voted_classifier.confidence(testing_set[3][0])*100)
-print("Classification:", voted_classifier.classify(testing_set[4][0]), "Confidence %:",voted_classifier.confidence(testing_set[4][0])*100)
-print("Classification:", voted_classifier.classify(testing_set[5][0]), "Confidence %:",voted_classifier.confidence(testing_set[5][0])*100)
+def sentiment(text):
+    feats = find_features(text)
+    return voted_classifier.classify(feats),voted_classifier.confidence(feats)
+
+
+print(sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...so yea!"))
+print(sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
